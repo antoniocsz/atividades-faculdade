16667cce07878ceeccbf6a8a51f26b0204e6588b
Antonio Santana <antcesar.santana@gmail.com>
2016-08-02 14:48:17 -0300

    add

--- SOperacionais/soma.c
+++ SOperacionais/soma.c
@@ -7,8 +7,13 @@ int main(int argc, char *argv[]){
 	int i, qnt = atoi(argv[1]);
 	long soma = 0;
 	srand((unsigned)time(NULL));
-	for (i = 0; i < qnt; i++){
-		soma += rand();
+	
+	if (argc > 1){	
+		for (i = 0; i < qnt; i++){
+			soma += rand();
+			printf("s = %ld\n", soma);
+		}
+	} else{
 		printf("s = %ld\n", soma);
 	}
 	
